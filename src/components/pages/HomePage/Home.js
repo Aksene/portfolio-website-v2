8058f80fe5416fe093cb7527a113174266a4e8e5
Abdou <sene78@hotmail.com>
2022-05-09 15:49:40 -0400

    27th commit

--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -4,11 +4,20 @@ import { homeObjOne, homeObjTwo, homeProject3, homeAboutMe4 } from './Data'
 import MainHeroSection from '../../MainHeroSection'
 import AboutMeHeroSection from '../../AboutMeHeroSection'
 import ExperienceHeroSection from '../../ExperienceHeroSection'
+import { Helmet } from "react-helmet";
+
 
 
 function Home() {
     return (
         <>
+            <Helmet>   
+                <title>SLAYMASTER - Create Beauty Content That Slays, Hair Tutorial</title>
+                <meta 
+                    name="description" 
+                    content="Build an empowering community for #EVERYONES beauty journey through video, blogging, and other forms of media. CREATE BEAUTY CONTENT THAT SLAYS!" 
+                />
+            </Helmet>
             <MainHeroSection/>
             <AboutMeHeroSection {...homeAboutMe4} />
             <ExperienceHeroSection/>
