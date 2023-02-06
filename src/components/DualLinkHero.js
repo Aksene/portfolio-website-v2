3cd8d9a5aee2d98486d4478e0152eec4b6bf4bb2
Abdou <sene78@hotmail.com>
2023-02-05 22:49:17 -0500

    29th Commit

--- src/components/DualLinkHero.js
+++ src/components/DualLinkHero.js
@@ -7,7 +7,7 @@ const DualLinkHero = ({ pic1, pic2, button1, button2, link1, link2}) => {
     return (
         <div className="dualLink-hero">
             <div className="dualLink-col">
-                <img src={pic1} alt="pic1" className='dualLink-hero_img' />
+                <img src={pic1} alt="pic 1" className='dualLink-hero_img' />
                 <div className="dualLink-hero_btn">
                     <a href={link1} target="_blank" rel="noreferrer" >
                         <button type="submit" className="hero__btn"><h1>{button1}</h1></button>
@@ -15,7 +15,7 @@ const DualLinkHero = ({ pic1, pic2, button1, button2, link1, link2}) => {
                 </div>
             </div>
             <div className="dualLink-col">
-                <img src={pic2} alt="pic2" className='dualLink-hero_img' />
+                <img src={pic2} alt="pic 2" className='dualLink-hero_img' />
                 <div className="dualLink-hero_btn">
                     <a href={link2}>
                         <button type="submit" className="hero__btn"><h1>{button2}</h1></button>
